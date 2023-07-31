a8b291e5e1e7e8aed7578169e4046939e39c511c
John Steele <124938266+Gitwit22@users.noreply.github.com>
2023-07-31 18:10:34 -0500

    Add project files.

---
+++ OMBDSearch/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using Flurl.Http;
+using Microsoft.AspNetCore.Mvc;
+using OMBDSearch.Models;
+using System.Diagnostics;
+
+
+namespace OMBDSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        public IActionResult MovieNight() 
+        { 
+            return View(); 
+        
+        }
+
+        public IActionResult MovieSearch()
+        {
+            return View();
+
+        }
+
+        
+
+
+        [HttpGet]
+
+        public ActionResult MovieSearchForm(string Title)
+        {
+            string apiUri = "http://www.omdbapi.com/?apikey=5ad7ba55&t";
+            var apiTask = apiUri.GetJsonAsync<Movie>();
+            apiTask.Wait();
+            Movie result = apiTask.Result;
+
+            
+            return View("MovieSearch", result);
+        }
+
+
+
+        
+
+        [HttpPost]
+        public IActionResult MovieSearchResults(Movie Title)
+        {
+            return View("MovieSearch", Title);
+
+        }
+
+        [HttpGet]
+        public IActionResult MovieNightForm()
+        {
+
+
+            return View("MovieNight");
+        }
+
+        //[HttpPost]
+        //    public IActionResult MovieNightResult(string Title1, string Title2, string Title3)
+        //    {
+        //        List<Movie> SearchResults = new List<Movie>();
+        //        Title1.ToList(SearchResults);
+        //        Title2.ToList(SearchResults);
+        //        Title3.ToList(SearchResults);
+        //        return View("MovieNight");
+        //    }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OMBDSearch/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace OMBDSearch.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+
+       public string Poster { get; set; }
+    }
+}
